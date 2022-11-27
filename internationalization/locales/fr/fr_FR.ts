a80ad89a10aef7f99c5b9e37d74cd6bbe1de28bb
BrightTechnologies1 <silascyrax@gmail.com>
2022-11-27 22:55:35 +0100

    Setting up (2)

---
+++ internationalization/locales/fr/fr_FR.ts
@@ -0,0 +1,3 @@
+export const fr_FR = {
+  none: 'maintenant',
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+// pages/_app.js
+import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import theme from '../src/theme'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      {/* <Head></Head> */}
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default App
