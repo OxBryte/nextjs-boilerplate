90abb9a09b85649b78abc297d8473c0dc28cd95d
BrightTechnologies1 <silascyrax@gmail.com>
2023-02-28 21:27:07 +0100

    feat: about

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,14 @@ import { Box, Text } from '@chakra-ui/react';
 import Waitlist from '../src/components/waitlist';
 import Hero from '../src/components/hero';
 import Service from '../src/components/service';
+import About from '../src/components/about';
 
 export default function Home() {
   return (
     <PageLayout title='Home' description='My Boilerplate'>
       <Hero />
       <Service />
+      <About />
       <Waitlist />
     </PageLayout>
   );
---
+++ src/components/about/index.tsx
@@ -0,0 +1,26 @@
+import { Stack, Tag, VStack, TagLabel, Heading, Text, Box, Button } from '@chakra-ui/react'
+import React from 'react'
+import ContainLayout from '../page-layout/container'
+import Bg from '../assets/frame.png'
+import Image from 'next/image'
+
+export default function About() {
+    return (
+        <ContainLayout>
+            <Stack direction={{ base: 'column', md: 'column', lg: 'row' }} align='center' justify='space-around' color='white' py='120px' >
+                <Box>
+                    <Image src={Bg} alt='' width={500} />
+                </Box>
+                <VStack align='left' gap='10px' maxW='550px'>
+                    <Tag colorScheme='cyan' borderRadius='full' size='md' w='fit-content'>
+                        <TagLabel>
+                            About us
+                        </TagLabel>
+                    </Tag>
+                    <Heading fontSize={['38px', '48px']} fontWeight='700' maxW='400px'>Attract new clients through engaging video</Heading>
+                    <Text maxW='350px'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit ame</Text>
+                </VStack>
+            </Stack>
+        </ContainLayout>
+    )
+}
